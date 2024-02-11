3e78043f618340bb7d8696beeafa35e284208bfa
RIE ONO <rieono2020@gmail.com>
2024-02-11 22:38:09 +0900

    second commit

--- src/main/java/com/example/demo/PortfolioAppApplication.java
+++ src/main/java/com/example/demo/PortfolioAppApplication.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortfolioAppApplication {
-
+public class PortfolioAppApplication extends SpringBootServletInitializer{
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PortfolioAppApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioAppApplication.class, args);
 	}
